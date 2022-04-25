e93ecb842ed573d1fa3ddb851408d769eb07456d
JamesRivers <rivers1980@gmail.com>
2022-04-25 21:03:47 +0100

    added the sh script for the dotfiles and the .gitconfig

---
+++ dotfile_links.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+ln -sf ~/mac_dotfiles/.zshrc ~/.zshrc
+ln -sf ~/mac_dotfiles/.viminfo ~/.viminfo
+ln -sf ~/mac_dotfiles/.vimrc ~/.vimrc
+ln -sf ~/mac_dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/mac_dotfiles/.gitconfig ~/.gitconfig
