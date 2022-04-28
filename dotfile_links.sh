66108790511c26e85228068d9944fde81f59b44e
JamesRivers <rivers1980@gmail.com>
2022-04-28 17:04:36 +0100

    moved on mac mini

--- dotfile_links.sh
+++ dotfile_links.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
-ln -sf ~/mac_dotfiles/.zshrc ~/.zshrc
-ln -sf ~/mac_dotfiles/.viminfo ~/.viminfo
-ln -sf ~/mac_dotfiles/.vimrc ~/.vimrc
-ln -sf ~/mac_dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/mac_dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/GitFiles/mac_dotfiles/.zshrc ~/.zshrc
+ln -sf ~/GitFiles/mac_dotfiles/.viminfo ~/.viminfo
+ln -sf ~/GitFiles/mac_dotfiles/.vimrc ~/.vimrc
+ln -sf ~/GitFiles/mac_dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/GitFiles/mac_dotfiles/.gitconfig ~/.gitconfig
