f4429eaae704ada26da866d20d13b4898d301411
JamesRivers <rivers1980@gmail.com>
2022-05-13 17:30:19 +0100

    zsh shell for zet new article

---
+++ new_article.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+echo New article file name...
+read artvar
+cp ~/GitFiles/academy/templates/note2.md .
+mv note2.md  $(date '+%Y%m%d%H%M')_$artvar.md
+sed -i '' -e "s+Title : testing_template+Title : $artvar+g" $(date '+%Y%m%d%H%M')_$artvar.md
+sed -i '' -e "s+datecreate+$(date '+%Y-%m-%d')+g" $(date '+%Y%m%d%H%M')_$artvar.md
+
