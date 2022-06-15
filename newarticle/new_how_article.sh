ed7b8fa50b7aa90942bd81b8da09a7e79b9629bd
JamesRivers <rivers1980@gmail.com>
2022-06-15 06:42:45 +0100

    edits

--- newarticle/new_how_article.sh
+++ newarticle/new_how_article.sh
@@ -7,4 +7,4 @@ cp ~/GitFiles/mac_dotfiles/newarticle/note2.md .
 mv note2.md  $(date '+%Y%m%d%H%M')_how_$artvar.md
 sed -i '' -e "s+Title : testing_template+Title : how_$artvar+g" $(date '+%Y%m%d%H%M')_how_$artvar.md
 sed -i '' -e "s+datecreate+$(date '+%Y-%m-%d')+g" $(date '+%Y%m%d%H%M')_how_$artvar.md
-vim $(date '+%Y%m%d%H%M')_exp_$artvar.md
+vim $(date '+%Y%m%d%H%M')_how_$artvar.md
