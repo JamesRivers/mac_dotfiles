65351c4c2a06f6cf2136d58c14ba5eb4bceb4b33
JamesRivers <rivers1980@gmail.com>
2022-06-15 06:07:59 +0100

    update

--- newarticle/new_exp_article.sh
+++ newarticle/new_exp_article.sh
@@ -7,4 +7,5 @@ cp ~/GitFiles/mac_dotfiles/newarticle/note2.md .
 mv note2.md  $(date '+%Y%m%d%H%M')_exp_$artvar.md
 sed -i '' -e "s+Title : testing_template+Title : exp_$artvar+g" $(date '+%Y%m%d%H%M')_exp_$artvar.md
 sed -i '' -e "s+datecreate+$(date '+%Y-%m-%d')+g" $(date '+%Y%m%d%H%M')_exp_$artvar.md
+vim $(date '+%Y%m%d%H%M')_exp_$artvar.md
 
