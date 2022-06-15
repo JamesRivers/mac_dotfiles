52c9b0bc5f9138c7a87d5ca7e00176e90fd1654f
JamesRivers <rivers1980@gmail.com>
2022-06-15 06:26:57 +0100

    edit to new artcles

--- newarticle/new_ref_article.sh
+++ newarticle/new_ref_article.sh
@@ -7,4 +7,4 @@ cp ~/GitFiles/mac_dotfiles/newarticle/note2.md .
 mv note2.md  $(date '+%Y%m%d%H%M')_ref_$artvar.md
 sed -i '' -e "s+Title : testing_template+Title : ref_$artvar+g" $(date '+%Y%m%d%H%M')_ref_$artvar.md
 sed -i '' -e "s+datecreate+$(date '+%Y-%m-%d')+g" $(date '+%Y%m%d%H%M')_ref_$artvar.md
-
+vim $(date '+%Y%m%d%H%M')_exp_$artvar.md
