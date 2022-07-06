559bc35544ac168946efb544a245a99b6f1adf30
JamesRivers <rivers1980@gmail.com>
2022-07-06 08:59:42 +0100

    Updated Wed  6 Jul 2022 08:59:42 BST

--- tmuxstart.sh
+++ tmuxstart.sh
@@ -4,20 +4,17 @@ session="rivers-term"
 tmux new-session -d -s $session
 
 window=0
-tmux rename-window -t $session:$window 'HOME'
-
+tmux rename-window -t $session:$window 'ZET'
+tmux send-keys -t $session:$window 'cd ~/GitFiles/zet/' C-m 
 window=1
-tmux new-window -t $session:$window -n 'LYNX'
-tmux send-keys -t $session:$window '? https://duckduckgo.com/' C-m 
-
+tmux new-window -t $session:$window -n 'SSH'
+tmux send-keys -t $session:$window 'fast-ssh' C-m 
 window=2
 tmux new-window -t $session:$window -n 'VPN'
 tmux send-keys -t $session:$window 'echo onvpn for imagine vpn or onwg for home' C-m
-
 window=3
 tmux new-window -t $session:$window -n 'CONTENT_DEV'
 tmux send-keys -t $session:$window 'cd ~/GitFiles/academy' C-m
-
 window=4
 tmux new-window -t $session:$window -n 'BTOP'
 tmux send-keys -t $session:$window 'btop' C-m
