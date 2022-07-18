783803cf056e7cd3594cfdab1a7dbf7460f9b17b
JamesRivers <rivers1980@gmail.com>
2022-07-18 18:42:14 +0100

    ripgrep fz case sensitve update, thank god

--- rg_bat_fzf.sh
+++ rg_bat_fzf.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
-RG_PREFIX="rg --files-with-matches"
+RG_PREFIX="rg -i --files-with-matches"
 INITIAL_QUERY=""
 FZF_DEFAULT_COMMAND="$RG_PREFIX '$INITIAL_QUERY'" 
   CHOICE=$(fzf --bind "change:reload:$RG_PREFIX {q} || true" \
       --sort \
       --multi \
-      --preview '[[ ! -z {} ]] && rg --pretty --context 5 {q} {}' \
+      --preview '[[ ! -z {} ]] && rg -i --pretty --context 5 {q} {}' \
       --ansi --phony --query "$INITIAL_QUERY") 
 
 [ -n "$CHOICE" ] && vim "$CHOICE"
